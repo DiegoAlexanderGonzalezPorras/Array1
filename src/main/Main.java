5f1763beb0db5d093e92141c238b733112b326f1
Diego Gonzalez <dagp0420@gmail.com>
2023-03-15 13:42:02 -0500

    Completado tercer problema

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -16,11 +17,15 @@ public class Main {
 		Integer[] array1 = { 70, 7, 5, 4, 3, 2, 7, 7, 29, 1 };
 		List<Integer> lista1 = Arrays.asList(array1);
 		
-		Integer[] array2 = { -9, -7, -5, -3, -1, 2, 4, 6, 8 };
+		Integer[] array2 = { -6, -5, 0, 5, 6};
 		List<Integer> lista2 = Arrays.asList(array2);
+		
+		Integer[] array3 = { 1, 5, 1, 1, 1, 10, 15, 20, 100};
+		List<Integer> lista3 = Arrays.asList(array3);
 
 		O(lista1, S);
 		Cuadrados(lista2, S);
+		monedas(lista3);
 	}
 
 	private static void O(List<Integer> lista, int S) {
@@ -56,40 +61,59 @@ public class Main {
 		List<Integer> NuevaLista = new ArrayList<>();
 		int limite = (int) Math.sqrt(S *10 + S);
 		
-		if (lista.get(0) < 0) {
-			boolean Primernegativo = false;
-			for (int i = lista.size()-1; i >= 0; i--) {
-				if (lista.get(i) < 0) {
-					if (!Primernegativo) {
-						ListaOrdenada.addAll(lista.subList(i + 1, lista.size()));
-						Primernegativo = true;
-					}
-					int numero = lista.get(i)*-1;
-					if (ListaOrdenada.size() > 0) {
-						for (int j = 0; j < ListaOrdenada.size(); j++) {
-							if (j == ListaOrdenada.size() - 1) {
-								ListaOrdenada.add(numero);
-								break;
-							}else if (numero <= ListaOrdenada.get(j)) {
-								ListaOrdenada.add(j, numero);
-								break;
-							}
+		if (lista.size() > 0) {
+			if (lista.get(0) < 0) {
+				boolean Primernegativo = false;
+				for (int i = lista.size()-1; i >= 0; i--) {
+					if (lista.get(i) < 0) {
+						if (!Primernegativo) {
+							ListaOrdenada.addAll(lista.subList(i + 1, lista.size()));
+							Primernegativo = true;
 						}
-					}else
-						ListaOrdenada.add(numero);
+						int numero = lista.get(i)*-1;
+						if (ListaOrdenada.size() > 0) {
+							for (int j = 0; j < ListaOrdenada.size(); j++) {
+								if (j == ListaOrdenada.size() - 1) {
+									ListaOrdenada.add(numero);
+									break;
+								}else if (numero <= ListaOrdenada.get(j)) {
+									ListaOrdenada.add(j, numero);
+									break;
+								}
+							}
+						}else
+							ListaOrdenada.add(numero);
+					}
 				}
+			}else
+				ListaOrdenada = lista;
+				
+			for (Integer numero: ListaOrdenada) {
+				if (numero <= limite)
+					NuevaLista.add((int)Math.pow(numero, 2));
+				else
+					break;
 			}
-		}else
-			ListaOrdenada = lista;
-			
-			
-		for (Integer numero: ListaOrdenada) {
-			if (numero <= limite)
-				NuevaLista.add((int)Math.pow(numero, 2));
-			else
-				break;
 		}
 		
 		System.out.println(NuevaLista);
+	}
+
+	private static void monedas(List<Integer> lista) {
+		int vueltas = 1;
+		Collections.sort(lista);
+		
+		if (lista.size() > 0 && lista.get(0) == 1) {
+			for (int i = 1; i < lista.size(); i++) {
+				if (vueltas >= lista.get(i) - 1)
+					vueltas += lista.get(i);
+				else
+					break;
+			}
+		}
+		
+		vueltas++;
+		
+		System.out.println(vueltas);
 	}
 }
